46ec8afe5471f937e29f1156cc5b3cc0e27796db
lequangtrai <69144612+lequangtrai@users.noreply.github.com>
2023-05-16 13:34:23 +0700

    upload test suite

---
+++ test-suites/test-suites/abctestSuite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abctestSuite</name>
+    <testSuiteGuid>a5264797-806f-4e3b-b7cd-aec01382318d</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/abc/abc</testCaseId>
+        <guid>b3f1a95d-06cb-42d4-9f31-58a10179ac90</guid>
+    </testCaseLink>
+</TestSuiteEntity>
