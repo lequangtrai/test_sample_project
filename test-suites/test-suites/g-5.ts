9837b264f3ecc028e867db6e15f8306557f9e6a1
lequangtrai <69144612+lequangtrai@users.noreply.github.com>
2023-06-16 15:37:27 +0700

    add test case to master

---
+++ test-suites/test-suites/g-5.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>g-5</name>
+    <testSuiteGuid>1d9726ba-3a7b-4fc5-adc4-330ac7f258cc</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/abc/abc</testCaseId>
+        <guid>9c34df9b-584b-493b-ad9d-652e13470c05</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/abc/abc</testCaseId>
+        <guid>a371ae94-631f-4a32-88cd-efa33781d80f</guid>
+    </testCaseLink>
+</TestSuiteEntity>
